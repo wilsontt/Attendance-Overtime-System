5b146a76a4fe3795ed15ded9a7fe48ee2ffdfd76
wilsontt <wilsontt@icloud.com>
2025-12-09 15:15:00 +0800

    修改版號

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -151,7 +151,7 @@ const HomePage: React.FC = () => {
           alt="Company Logo" 
           style={{ height: '60px', marginRight: '15px' }} 
         />
-        <h1 style={{ margin: 0 }}>出勤加班單系統</h1>
+        <h1 style={{ margin: 0 }}>出勤加班單系統 v1.2.0</h1>
       </div>
       <FileUploader onFileProcessed={handleFileProcessed} />
 
